b72fe2c059e76a39ced1b2b8c3ad176d4fd9926e
Zoltan Dobrovolni <zoltan.dobrovolni@kuka.com>
2022-02-27 20:40:52 +0100

    Add file upload functionlity

---
+++ components/FileUpload.tsx
@@ -0,0 +1,58 @@
+import React, { FC } from "react";
+import { parseFile } from "../scripts/parseFile";
+
+type SelectFileEventHandler = (
+  event: React.ChangeEvent<HTMLInputElement>
+) => void;
+
+const FileUpload: FC = () => {
+  const onFileChange: SelectFileEventHandler = async (event) => {
+      const files = event.currentTarget?.files;
+      console.log(files);
+    if (files && files.length > 0) {
+      try {
+        const content = await readFileContent(files[0]);
+        parseFile(content);
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      console.log("No file found");
+    }
+  };
+
+  function readFileContent(file: File): Promise<string> {
+    const reader = new FileReader();
+    return new Promise((resolve, reject) => {
+      reader.onload = (event) => {
+        const content = event?.target?.result;
+        if (typeof content === "string") {
+          resolve(content);
+        } else {
+          reject("Content of the file is not type string.");
+        }
+      };
+      reader.onerror = (error) => reject(error);
+      reader.readAsText(file);
+    });
+  }
+
+  return (
+    <main>
+      <div>
+        <label htmlFor="file-upload">Select a txt file to parse</label>
+      </div>
+      <div>
+        <input
+          type="file"
+          id="file-upload"
+          name="filer"
+          accept=".txt"
+          onChange={onFileChange}
+        />
+      </div>
+    </main>
+  );
+};
+
+export default FileUpload;
---
+++ scripts/parseFile.ts
@@ -0,0 +1,5 @@
+export const parseFile = (content: string) => {
+    console.log(content);
+    // const lines = content.split("\r\n"); // sima /n-el így nem lesz jó gondolom. ki kell próbálni linux-ra konvertáltva
+    // console.log(lines);
+}
